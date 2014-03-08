4bcc6f7e8921e8876ed64f038916b2f0e7fdecfb
MrS0m30n3 <ytubedlg@gmail.com>
2014-03-08 14:50:46 +0200

    Upgrade about dialog

--- youtube_dl_gui/YoutubeDLGUI.py
+++ youtube_dl_gui/YoutubeDLGUI.py
@@ -508,8 +508,44 @@ class GeneralPanel(wx.Panel):
     dlg.Destroy()
     
   def OnAbout(self, event):
-    msg = TITLE + ' (GUI)'+ '\nVersion: ' + __version__ + '\nCreated by: Sotiris Papadopoulos'
-    wx.MessageBox(msg, 'About', wx.OK | wx.ICON_INFORMATION)
+    description = '''A cross platform front-end GUI of 
+the popular youtube-dl written in Python.'''
+    
+    license = '''This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>'''
+    
+    info = wx.AboutDialogInfo()
+    
+    info.SetIcon(wx.Icon('../icons/ytube.png', wx.BITMAP_TYPE_ICO))
+    info.SetName(TITLE)
+    info.SetVersion(__version__)
+    info.SetDescription(description)
+    info.SetWebSite('https://github.com/MrS0m30n3/youtube-dl-gui')
+    info.SetLicense(license)
+    info.AddDeveloper('Sotiris Papadopoulos')
+    wx.AboutBox(info)
     
   def load_options(self):
     self.savePathBox.SetValue(self.optList.savePath)

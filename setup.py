a6b13f81ce7e39462ec188fad998755a2b66d26c
MrS0m30n3 <ytubedlg@gmail.com>
2014-04-29 01:26:03 +0300

    Add icons on package install

--- setup.py
+++ setup.py
@@ -3,14 +3,26 @@
 from distutils.core import setup
 from youtube_dl_gui import version
 
-setup(name='Youtube-DLG',
+name = 'Youtube-DLG'
+desc = 'Youtube-dl GUI'
+ldesc = 'A cross platform front-end GUI of the popular youtube-dl written in wxPython'
+license = 'UNLICENSE'
+platform = 'Cross-Platform'
+author = 'Sotiris Papadopoulos'
+author_email = 'ytubedlg@gmail.com'
+project_url = 'http://mrs0m30n3.github.io/youtube-dl-gui/'
+packages = ['youtube_dl_gui']
+data_files = [('lib/python2.7/site-packages/youtube_dl_gui/icons', 
+		['youtube_dl_gui/icons/youtube-dl-gui.png'])]
+
+setup(name=name,
       version=version.__version__,
-      description='Youtube-dl GUI',
-      long_description='A cross platform front-end GUI of the popular youtube-dl written in wxPython.',
-      license='UNLICENSE',
-      platforms='cross-platform',
-      author='Sotiris Papadopoulos',
-      author_email='ytubedlg@gmail.com',
-      url='https://github.com/MrS0m30n3/youtube-dl-gui',
-      packages=['youtube_dl_gui'],
-      data_files=[('icons', ['youtube_dl_gui/icons/youtube-dl-gui.png'])])
+      description=desc,
+      long_description=ldesc,
+      license=license,
+      platforms=platform,
+      author=author,
+      author_email=author_email,
+      url=project_url,
+      packages=packages,
+      data_files=data_files)

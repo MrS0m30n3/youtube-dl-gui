c6fb3d284c4328ddd9149eb67a628d40f1cc2f41
Sergey M․ <dstftw@gmail.com>
2015-04-08 00:09:35 +0600

    [mainframe] Fixed center naming consistency and typo

--- youtube_dl_gui/mainframe.py
+++ youtube_dl_gui/mainframe.py
@@ -131,7 +131,7 @@ class MainFrame(wx.Frame):
         self.update_thread = None
         self.app_icon = get_icon_file()
 
-        self.Centre()
+        self.Center()
 
         # Create the app icon
         if self.app_icon is not None:
@@ -467,7 +467,7 @@ class MainFrame(wx.Frame):
         """Event handler of the self._options_btn widget.
 
         This method is used when the options button is pressed to show
-        the optios window.
+        the options window.
 
         """
         self._options_frame.load_all_options()

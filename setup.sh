48c5c7680100d08126b18502df5b0a0bd4464604
oleksis <oleksis.fraga@gmail.com>
2020-08-09 13:52:45 -0400

    Install wget in Centos 7 for Docker Action

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 # Fail on errors.
 set -e
 
+# Install wget in Centos 7
+yum install -y wget
+
 # Install pre-requirements like wxPython
 # on PyInstaller ManyLinux 2014 Docker Action
 wget https://extras.wxpython.org/wxPython4/extras/linux/gtk3/centos-7/wxPython-4.1.0-cp36-cp36m-linux_x86_64.whl

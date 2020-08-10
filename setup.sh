23a4aafec2c8e0be23075b92d46a9d1b1956c189
oleksis <oleksis.fraga@gmail.com>
2020-08-10 08:42:19 -0400

    Build Translations

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ yum install -y wget
 # on PyInstaller ManyLinux 2014 Docker Action
 wget https://extras.wxpython.org/wxPython4/extras/linux/gtk3/centos-7/wxPython-4.1.0-cp36-cp36m-linux_x86_64.whl
 pip3 install wxPython-4.1.0-cp36-cp36m-linux_x86_64.whl
+# Install requirements here
+pip3 install -r requirements.txt
+# Build Translations
+python3 setup.py build_trans
